9f7c247962f887a20782c7ac00caaf02881fcd78
Gennady Artamonov <gartamonov@colvir.ru>
2020-02-11 22:08:20 +0300

    Refactored form builder and cleanup methods in ShopDialog

--- src/app/pages/main-page/components/shop-dialog/shop-dialog.component.ts
+++ src/app/pages/main-page/components/shop-dialog/shop-dialog.component.ts
@@ -5,7 +5,9 @@ import {DadataAddress, DadataConfig} from "@kolkov/ngx-dadata";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MenuItem} from "primeng";
 import {AddressSuggestion} from "../../../../core/models/suggestion-address.model";
-import {ShopCounterparty, ShopModel, ShopType} from "../../../../core/models/shop.model";
+import {ShopAddress, ShopCounterparty, ShopModel, ShopType} from "../../../../core/models/shop.model";
+import * as _ from 'lodash';
+import {distinctUntilChanged, tap} from "rxjs/operators";
 
 
 @Component({
@@ -29,19 +31,13 @@ export class ShopDialogComponent implements OnInit, OnChanges {
   private newShop = {};
   private shopFrom: FormGroup;
   private addressSuggestion: AddressSuggestion;
-
+  private shopClone: ShopModel;
+  private saveShop: ShopModel;
+  private changedFields: any;
 
 
   constructor(@Inject(FormBuilder) private fb: FormBuilder) {
-    this.shopFrom = this.fb.group({
-      'shopType': [{}, Validators.required],
-      'counterparty': [0, Validators.required],
-      'shopName': ['', Validators.required],
-      'shopActive': [true],
-      'shopAddress': [''],
-      'shopComment': ['']
-    });
-
+    this.shopFrom = this.buildShopForm();
   }
 
   ngOnInit() {
@@ -54,50 +50,55 @@ export class ShopDialogComponent implements OnInit, OnChanges {
   }
 
   shopSaved() {
+    // console.dir(this.shopClone);
+    this.shopFrom.valueChanges.subscribe(
+      s => this.saveShop = s
+    );
+    this.getUpdatedFields(this.shopFrom);
 
-    if (this.isNew) {
-
-      this.newShop = {
-        ...this.newShop,
-        shopData: {
-          name: this.shopFrom.get('shopName').value,
-          active: this.shopFrom.get('shopActive').value,
-          counterparty: {id: this.shopFrom.get('counterparty').value['id']},
-          shopTypes: [{id: this.shopFrom.get('shopType').value['id']}],
-          address: {
-            active: true,
-            address: this.addressSuggestion.value,
-            comment: this.shopFrom.get('shopComment').value,
-            suggestion: this.addressSuggestion
-          }
-        },
-      };
-      this.onNewShopSaved.emit(this.newShop);
-    } else {
-      this.newShop = {
-        ...this.newShop,
-        shopData: {
-          name: this.shopFrom.get('shopName').value,
-          id: this.shop.id,
-          active: this.shopFrom.get('shopActive').value,
-          counterparty: {id: this.shopFrom.get('counterparty').value['id']},
-          version: this.shop.version,
-          shopTypes: [{id: this.shopFrom.get('shopType').value['id']}],
-          address: {
-            id: this.shop.address.id,
-            active: this.shop.address.active,
-            address: this.addressSuggestion.value,
-            version: this.shop.address.version,
-            comment: this.shopFrom.get('shopComment').value,
-            suggestion: this.addressSuggestion
-          }
-        }
-
-      };
-      this.onEditedShopSave.emit(this.newShop);
-    }
-
-    this.onCloseDialog.emit(false);
+    // if (this.isNew) {
+    //
+    //   this.newShop = {
+    //     ...this.newShop,
+    //     shopData: {
+    //       name: this.shopFrom.get('shopName').value,
+    //       active: this.shopFrom.get('shopActive').value,
+    //       counterparty: {id: this.shopFrom.get('counterparty').value['id']},
+    //       shopTypes: [{id: this.shopFrom.get('shopType').value['id']}],
+    //       address: {
+    //         active: true,
+    //         address: this.addressSuggestion.value,
+    //         comment: this.shopFrom.get('shopComment').value,
+    //         suggestion: this.addressSuggestion
+    //       }
+    //     },
+    //   };
+    //   this.onNewShopSaved.emit(this.newShop);
+    // } else {
+    //   this.newShop = {
+    //     ...this.newShop,
+    //     shopData: {
+    //       name: this.shopFrom.get('shopName').value,
+    //       id: this.shop.id,
+    //       active: this.shopFrom.get('shopActive').value,
+    //       counterparty: {id: this.shopFrom.get('counterparty').value['id']},
+    //       version: this.shop.version,
+    //       shopTypes: [{id: this.shopFrom.get('shopType').value['id']}],
+    //       address: {
+    //         id: this.shop.address.id,
+    //         active: this.shop.address.active,
+    //         address: this.addressSuggestion.value,
+    //         version: this.shop.address.version,
+    //         comment: this.shopFrom.get('shopComment').value,
+    //         suggestion: this.addressSuggestion
+    //       }
+    //     }
+    //
+    //   };
+    // //   this.onEditedShopSave.emit(this.newShop);
+    // }
+    //
+    // this.onCloseDialog.emit(false);
   }
 
 
@@ -107,44 +108,77 @@ export class ShopDialogComponent implements OnInit, OnChanges {
 
   }
 
+  getUpdatedFields(form: any) {
+    let dirtyValues = {};
+    Object.keys(form.controls).forEach(k => {
+      let currentControl = form.controls[k];
+      if (currentControl.dirty) {
+        if (currentControl.controls)
+          dirtyValues[k] = this.getUpdatedFields(currentControl);
+        else
+          dirtyValues[k] = currentControl.value;
+      }
+    });
+    console.dir(dirtyValues);
+  }
 
-  initAfterViewFormValues(fields: { [key: string]: any }[]): void {
+  initAfterShowFormValues(fields: { [key: string]: any }[]): void {
     fields.forEach(field => {
       this.shopFrom.patchValue({...field});
     })
   }
 
-  newResetForm(): void {
-    this.initAfterViewFormValues([
-      {'shopType': null},
-      {'counterparty': null},
-      {'shopName': null},
-      {'shopActive': true}
-    ]);
-  }
-
-
 
   onAddressSuggest(e): void {
-    if(e){
+    if (e) {
       this.addressSuggestion = e;
-    }else{
+    } else {
       this.addressSuggestion = <AddressSuggestion>{}
     }
 
   }
 
+  buildShopForm(): FormGroup {
+    return this.fb.group({
+      id: null,
+      name: ['', Validators.required],
+      shopTypes: [<ShopType[]>[], Validators.required],
+      counterparty: [<ShopCounterparty>{}, Validators.required],
+      active: true,
+      address: this.fb.group({
+        id: null,
+        version: null,
+        active: true,
+        address: '',
+        comment: ''
+      }),
+      version: null,
+      updatedFields: null,
+    });
+  }
+
   afterShow() {
     if (!this.isNew) {
-      this.initAfterViewFormValues([
-        {'shopType': this.shop.shopTypes[0]},
-        {'counterparty': this.shop.counterparty},
-        {'shopName': this.shop.name},
-        {'shopActive': this.shop.active},
-        {'shopComment': this.shop.address.comment}
+      this.initAfterShowFormValues([
+        {id: this.shop.id},
+        {name: this.shop.name},
+        {shopTypes: this.shop.shopTypes[0]},
+        {counterparty: this.shop.counterparty},
+        {
+          address: {
+            id: this.shop.address.id,
+            version: this.shop.address.version,
+            active: this.shop.address.active,
+            address: this.shop.address.address,
+            comment: this.shop.address.comment
+          },
+          version: this.shop.version,
+          updatedFields: null
+        },
       ]);
+      this.shopClone = this.shopFrom.value;
     } else {
-      this.newResetForm();
+      this.shopFrom.reset();
     }
 
 
