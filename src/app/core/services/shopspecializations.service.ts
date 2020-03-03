b2effeea5cd11c44a599f328c80e020b84a4b438
andreyfomin <fomin.andre@gmail.com>
2020-03-03 12:52:33 +0200

    add shop specializations table component

--- src/app/core/services/shopspecializations.service.ts
+++ src/app/core/services/shopspecializations.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import {Conf} from '../../../assets/config/conf';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopspecializationsService {
 
-  constructor() { }
+  private config = new Conf();
+  private srvUrl: string = this.config.BASE_URL + this.config.SHOP_SPECIALIZATION_URL;
+
+  constructor(
+    private http: HttpClient) {
+  }
+
+  fetchData(options = {}): Observable<any> {
+    return this.http.get(
+      this.srvUrl,
+      {
+        params: {...options}
+      });
+  }
+
+  editItem(updateData: {}, id: number): Observable<any> {
+    return this.http.patch(`${this.srvUrl}/${id}`, updateData)
+  }
+
+  newItem(shopData: {}): Observable<any> {
+    return this.http.post(this.srvUrl, shopData);
+  }
+
 }
--- src/app/pages/main-page/components/shop-specializations-data-table/shop-specializations-data-table.component.ts
+++ src/app/pages/main-page/components/shop-specializations-data-table/shop-specializations-data-table.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
+import {MessageService} from 'primeng';
+import {ConfigService} from '../../../../core/services/config.service';
+import {AppTableTypes} from '../../../../core/models/app-tabe-types.enum';
+import {Observable} from 'rxjs';
+import {AppDataTableModel} from '../../../../core/models/app-data-table.model';
+import {ShopspecializationsService} from '../../../../core/services/shopspecializations.service';
 
 @Component({
   selector: 'app-shop-specializations-data-table',
   templateUrl: './shop-specializations-data-table.component.html',
   styleUrls: ['./shop-specializations-data-table.component.scss']
 })
-export class ShopSpecializationsDataTableComponent implements OnInit {
+export class ShopSpecializationsDataTableComponent extends AppDataTableModel implements OnInit {
 
-  constructor() { }
+  selectedItem: any;
 
-  ngOnInit(): void {
+  constructor(
+    messageService: MessageService,
+    injector: Injector,
+    configService: ConfigService,
+    shopspecializationsService: ShopspecializationsService) {
+    super(messageService,
+      configService,
+      shopspecializationsService);
   }
 
+  ngOnInit(): void {
+    this.loading = true;
+    this.loadShopsTableHeaders(
+      AppTableTypes.SHOP_SPECIALIZATIONS_TABLE_TYPE);
+    this.initColumnFilter(() => {
+      return []
+    });
+  }
+
+  fetchFilterData(params: Object, fieldName: string): Observable<any> {
+    return this.tableDataService.fetchData(params);
+  }
 }
