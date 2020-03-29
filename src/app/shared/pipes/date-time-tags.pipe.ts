e9b6c7784372bdf5c8e7e67f8ef1f2db68b1d304
andreyfomin <fomin.andre@gmail.com>
2020-03-29 19:05:06 +0300

    fix date column filter

--- src/app/shared/pipes/date-time-tags.pipe.ts
+++ src/app/shared/pipes/date-time-tags.pipe.ts
@@ -1,14 +1,28 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import * as moment from 'moment';
+import moment from 'moment-timezone';
+import {ConfigService} from '../../core/services/config.service';
 
 @Pipe({
   name: 'dateTimeTags'
 })
 export class DateTimeTagsPipe implements PipeTransform {
+  constructor(private configService: ConfigService) {
+  }
 
+  /**
+   * Метод вызывается из thml темплейта использует библиотеку
+   * [moment.js](https://momentjs.com/docs/) для построения отформатированной даты
+   * Пример:
+   * - moment.tz("2013-12-01","US/Pacific").locale('ru').format('DD.MM.YYYY')
+   * @param {string} value - ISO8601 date string [moment ISO8601](https://momentjs.com/docs/#/parsing/string/)
+   * @param args
+   */
   transform(value: any, ...args: any[]): string {
     if(moment(value, moment.ISO_8601).isValid()) {
-      return moment(value, "MMM YYYY", 'ru').format("DD MMM YYYY");
+      return moment
+        .tz(value,this.configService.fetchDateTimeConfig().tz)
+        .locale(this.configService.fetchDateTimeConfig().locale)
+        .format(this.configService.fetchDateTimeConfig().format);
     } else {
       return value
     }
