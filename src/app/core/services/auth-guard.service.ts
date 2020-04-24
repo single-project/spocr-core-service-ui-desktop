091a793dc3d190a0a75f977e874a20659e50822d
akurutin <akurutin@gk21.ru>
2020-04-24 12:03:53 +0300

    added auth guard to main(fixed missing service)

---
+++ src/app/core/services/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  Router} from "@angular/router";
+import {AuthService} from "./auth.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService  implements CanActivate {
+
+  constructor(private authService: AuthService, public router: Router) { }
+
+  canActivate() {
+    if (!this.authService.authorized) {
+      this.authService.logout();
+      location.reload(true);
+      return false;
+    }
+    return true;
+  }
+}
